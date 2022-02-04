79a53ccb99b40122551ebf1cf7ddcc4d2b9f49f3
Sarvesh Nagar <sarveshnagar08@gmail.com>
2022-02-04 17:24:49 +0530

    base config

---
+++ src/main/java/io/word/guessmedata/GuessMeDataApplication.java
@@ -0,0 +1,13 @@
+package io.word.guessmedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessMeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessMeDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/word/guessmedata/GuessMeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.word.guessmedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessMeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
